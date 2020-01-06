330380556b81f9c8f052eab63621bfd36a20ff89
Rorschach-zh <z978766951@gmail.com>
2020-01-06 10:47:44 +0800

    代码优化：学生映射类的参数修复         删除学生信息页面正则表达式约束         把学生实体类的studyid更改为String类型         闭合input标签

--- src/main/java/com/tujiao/controller/LoginController.java
+++ src/main/java/com/tujiao/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.tujiao.controller;
 
 import com.tujiao.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
